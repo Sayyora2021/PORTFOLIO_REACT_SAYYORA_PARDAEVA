87c1f60305f24ad83921ec15935c6b5f07f66097
Sayyora2021 <sayyorapardaeva@gmail.com>
2023-01-13 16:45:08 +0100

    description-img-ok

---
+++ src/components/Competence/Infoportfolio.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Infoportfolio = ({ title, description }) => {
+  return <div></div>;
+};
+
+export default Infoportfolio;
--- src/components/Competence/Portfolio.js
+++ src/components/Competence/Portfolio.js
@@ -13,12 +13,16 @@ const Portfolio = () => {
 
       {projects.map((project, index) => (
         <div key={index} className="project" id="p1">
-          <img src={project.cover} alt="projets" />
+          <img src={project.cover} alt={project.alt} />
+          <div className="info">
+            {project.title}
+            <p>{project.description}</p>
+            <a href={project.site}> Site du projet</a>
+            <a herf={project.git}> Code du projet</a>
+          </div>
         </div>
       ))}
-      {projects.map((item, index) => (
-        <div key={item} className="info"></div>
-      ))}
+
       <Hobbies />
     </section>
   );
